263f891ebcbef356a1c6869a0aa75a01bc437ae2
JeftaM <136071987+JeftaM@users.noreply.github.com>
2023-06-23 15:43:45 +0200

    1543

--- jefta.py
+++ jefta.py
@@ -1,16 +1,16 @@
 import pandas
 
 def methodejefta():
-  bestand = pandas.read_csv("nyc-jobs.csv")
-  for i,country in bestand.iterrows():
-    if(country["Country Code"]==landcode):
-      pass
-    else
-      bestand=bestand.drop(i)
+  # bestand = pandas.read_csv("nyc-jobs.csv")
+  # for i,country in bestand.iterrows():
+  #   if(country["Country Code"]==landcode):
+  #     pass
+  #   else
+  #     bestand=bestand.drop(i)
 
-  result=bestand.to_json(orient="records")
-  parsed=loads(result)
-  return dumps(parsed,indent=4)
+  # result=bestand.to_json(orient="records")
+  # parsed=loads(result)
+  # return dumps(parsed,indent=4)
   return "dit komt van jefta"
 
 # print(methodefelix("AFG"))
