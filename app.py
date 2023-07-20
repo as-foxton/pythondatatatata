5f663022f8081f74cafc5b85dc7132c6411421cb
JeftaM <136071987+JeftaM@users.noreply.github.com>
2023-07-20 08:38:59 +0200

    2007

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 from flask_cors import CORS
 from flask import Response
-# import felix
-import jefta
-# import ronald
-import io
-from matplotlib.figure import Figure
-# from dotenv import load_dotenv
-import os
-# import requests
-# load_dotenv()
+# import jefta
+# import io
+# from matplotlib.figure import Figure
+# import os
+import pandas as pd
 
+from json import loads, dumps
 
 
 app = Flask(__name__)
@@ -20,27 +17,73 @@ CORS(app)
 def helloWorld():
   return "Hello, cross-origin-world!"
 
+
+
+# @app.route("/jefta/<ldcode>")
+# # jobid=int(jobid)
+# def helloJefta(ldcode):
+#   # fig, ax = jefta.methodejefta()  # Assuming this generates a plot and returns the figure and axes
+
+#   # output = io.BytesIO()
+#   # fig.savefig(output, format='png')
+
+#   # return Response(output.getvalue(), mimetype='image/png')
+
+#   return jefta.methodejefta(ldcode)
+
+
+
+# country="DE"
+# sex="F"
+# def filter_data(country,sex):
+
+
+@app.route('/filterdata', methods=['GET'])
+def filter_data():
+
+  df_filtered = pd.read_csv("df_filtered.csv")
+  country = request.args.get('country')
+  sex = request.args.get('sex')
+  # country="DE"
+  # sex="F"
+  # print(type(country))
+  df_filtered2 = df_filtered[(df_filtered['geo'] == country) & (df_filtered['sex'] == sex)]
+  result = df_filtered2.to_json(orient="records")
+  parsed = loads(result)
+  # print(f"Country: {country}")
+  # print(f"Sex: {sex}")
+  # print(f"Filtered Data: {df_filtered2}")
+
+  return dumps(parsed, indent=4)
+
+
+# print(filter_data(country,sex))
+
+
+# def helloJefta():
+#   # fig, ax = jefta.methodejefta()  # Assuming this generates a plot and returns the figure and axes
+
+#   # output = io.BytesIO()
+#   # fig.savefig(output, format='png')
+
+#   # return Response(output.getvalue(), mimetype='image/png')
+
+#   return jefta.methodejefta(ldcode)
+
+
+
+
+
+# @app.route('/')
+# def home():
+#     with open('api_key.txt', 'r') as f:
+#         api_key = f.read().strip()
+#     return render_template('vacature1.html', api_key=api_key)
+
 # @app.route("/felix/<landcode>")
 # def helloFelix(landcode):
 #   return felix.methodefelix(landcode)
 
-@app.route("/ronald")
-def helloRonald():
-  return ronald.methoderonald()
-
-@app.route("/jefta")
-# jobid=int(jobid)
-def helloJefta():
-  fig, ax = jefta.methodejefta()  # Assuming this generates a plot and returns the figure and axes
-
-  output = io.BytesIO()
-  fig.savefig(output, format='png')
-
-  return Response(output.getvalue(), mimetype='image/png')
-
-@app.route('/')
-def home():
-    with open('api_key.txt', 'r') as f:
-        api_key = f.read().strip()
-    return render_template('vacature1.html', api_key=api_key)
-
+# @app.route("/ronald")
+# def helloRonald():
+#   return ronald.methoderonald()
--- jefta.py
+++ jefta.py
@@ -1,15 +1,70 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-
-# from json import loads, dumps
-
-
+import matplotlib
+# import matplotlib.pyplot as plt
+# matplotlib.use('agg')
 
 
-def methodejefta():
-    df_filtered= pd.read_csv("df_filtered.csv")
+from json import loads, dumps
 
-    df_filtered = df_filtered.sort_values('year')
+
+
+
+# def methodejefta(ldcode):
+  # df_filtered= pd.read_csv("df_filtered.csv")
+
+#   df_filtered2 = df_filtered[
+ 
+#     (df_filtered['geo'] == ldcode) 
+  
+# ]
+
+  # result = df_filtered2.to_json(orient="records")
+  # parsed = loads(result)
+  # return dumps(parsed, indent=4) 
+
+
+
+# def filterdata():
+#       df_filtered= pd.read_csv("df_filtered.csv")
+
+# country = request.args.get('country')
+#     sex = request.args.get('sex')
+
+
+
+#  df_filtered2 = df_filtered[
+#     (df_filtered['geo'] == country) & (df_filtered['sex'] == sex)
+# ]
+
+#  result = df_filtered2.to_json(orient="records")
+#   parsed = loads(result)
+# return dumps(parsed, indent=4) 
+
+
+    # if country:
+    #     filtereddata = [x for x in filtereddata if user['country'] == country]
+
+    # if sex:
+    #     filtereddata = [x for x in filtereddata if user['sex'] == sex]
+
+  
+
+# df_filtered2
+
+
+# methodejefta(ldcode)
+
+# print(methodejefta("UK"))
+
+
+
+# for i, country in df_filtered.iterrows():
+    # if(country["CountryCode"]==  landcode):
+    #   pass
+    # else:
+    #   bestand = bestand.drop(i)
+
+    # df_filtered = df_filtered.sort_values('TIME_PERIOD')
 
 
     # Calculate the minimum and maximum employment values
@@ -24,16 +79,24 @@ def methodejefta():
     # plt.ylim([min_value, max_value])
 
     # Set the title and labels
-    df_filtered['year'] = pd.to_datetime(df_filtered['year']).dt.year
-    df_filtered['year'] = pd.to_datetime(df_filtered['year'], format='%Y')
-    fig, ax = plt.subplots(figsize=(10, 6))
-    ax.plot(df_filtered['year'], df_filtered['employment'])
 
-    ax.set_title('Employment over time for selected group')
-    ax.set_xlabel('Year')
-    ax.set_ylabel('Employment')
-    n = 1  # Choose every 10th label
-    [l.set_visible(False) for (i,l) in enumerate(ax.xaxis.get_ticklabels()) if i % n != 0]
+
+    # df_filtered['TIME_PERIOD'] = pd.to_datetime(df_filtered['TIME_PERIOD']).dt.year
+    # df_filtered['TIME_PERIOD'] = pd.to_datetime(df_filtered['TIME_PERIOD'], format='%Y')
+
+
+    # fig, ax = plt.subplots(figsize=(10, 6))
+    # ax.plot(df_filtered['TIME_PERIOD'], df_filtered['OBS_VALUE'])
+
+    # ax.set_title('Employment over time for selected group')
+    # ax.set_xlabel('Year')
+    # ax.set_ylabel('Employment')
+    # n = 1  # Choose every 10th label
+    # [l.set_visible(False) for (i,l) in enumerate(ax.xaxis.get_ticklabels()) if i % n != 0]
+
+
+
+  
 
 
     # Show the plot
@@ -48,7 +111,7 @@ def methodejefta():
 
 #   result=bestand.to_json(orient="records")
 #   parsed=loads(result)
-    return fig,ax
+    # return fig,ax
 
 # fig, ax = create_figure()
 
